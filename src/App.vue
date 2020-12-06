5924f1fdad85d5741d3269bfeb0c20e1388b7ca5
David Mateo <davidvegat17@gmail.com>
2020-12-05 23:10:04 -0500

    footer actualizado

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
+import PageFooter from './components/PageFooter.vue'
 
 export default {
+  components: { PageFooter },
   name: 'App',
 }
 </script>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <footer class="footer">
+        <div class="flex-container">
+            <div class="footer-container">
+                <h4>Motivación</h4>
+                <p>Este es el proyecto de el sprint numero 1 de la fase de desarrollo de software del grupo 182, el objetivo es dearrollar una pagina web estatica. </p>
+                <h4>enlace Github</h4>
+                <p><a class="white" href="https://github.com/andresromero222/semana-1-182">https://github.com/andresromero222/semana-1-182</a></p>
+            </div>
+            <div class="footer-container">
+                <h4>Contacto</h4>
+                <p>David Mateo Vega Triana: davidvegat17@gmail.com</p>
+                <p>Andrés Felipe Romero Silva: fiorenttos227@gmail.com</p>
+                <p>Santiago Puentes Ninco: sntiagopuentes@gmail.com</p>
+                <p>Laura Juliana Bohorquez Vergara: laujuliana14@gmail.com</p>
+                <p>Cristian Revelo Sanabria: cristianrevelo@hotmail.com</p>
+            </div>
+        </div>
+        <div class= "footer-form-container">
+            <form>
+                <div class="row">
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Nombre">
+                  </div>
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Correo">
+                  </div>
+    
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Mensaje">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Enviar</button>                                   
+                </div>
+                
+                </form>
+                
+        </div>
+        <div class="footer-copyright text-center py-1 bg-dark">© 2020 Copyright: DAD
+                 </div>
+    </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PageFooter",
+}
+</script>
+
+<style scoped>
+
+.footer{
+        background-color: grey;
+        
+    }
+    .footer-container{
+        padding: 20px;
+        color:gainsboro;
+        max-width: 600px;
+    }
+
+    .footer-form-container{
+        padding: 20px;
+        color:gainsboro;
+    }
+
+    .flex-container{
+        display: flex;
+    }
+
+    .white{
+        color:white;
+    }
+    .footer-copyright{
+        color: beige;
+    }
+
+</style>
