76f2f137e39bb8033dc16fae83ae00a4f5e82175
Andres Romero <aromero@tul.com.co>
2020-12-06 22:35:57 -0500

    Repositorio Bien

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
+    <page-footer></page-footer>
+
+    <h1>{{ title }}</h1>
   </div>
 </template>
 
 <script>
 
+
+import PageFooter from './components/PageFooter.vue'
+
 export default {
   name: 'App',
+  components:{
+    PageFooter
+  },
+  data(){
+    return {
+      title : 'Vivo dentro de VUE'
+    }
+  }
 }
 </script>
 
